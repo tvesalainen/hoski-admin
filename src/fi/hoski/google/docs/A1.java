33fe46bb9fb1d10c16276fd651b687c1a5c96cd2
tkv <timo.vesalainen@iki.fi>
2013-05-16 12:53:04 +0300

    Adding

---
+++ src/fi/hoski/google/docs/A1.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2012 Helsingfors Segelklubb ry
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package fi.hoski.google.docs;
+
+/**
+ *
+ * @author Timo Vesalainen
+ */
+public interface A1
+{
+    public static final int A = 0;
+    public static final int B = 1;
+    public static final int C = 2;
+    public static final int D = 3;
+    public static final int E = 4;
+    public static final int F = 5;
+    public static final int G = 6;
+    public static final int H = 7;
+    public static final int I = 8;
+    public static final int J = 9;
+    public static final int K = 10;
+    public static final int L = 11;
+    public static final int M = 12;
+    public static final int N = 13;
+    public static final int O = 14;
+    public static final int P = 15;
+    public static final int Q = 16;
+    public static final int R = 17;
+    public static final int S = 18;
+    public static final int T = 19;
+    public static final char[] LETTER = new char[]
+    {
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U'
+    };
+}
